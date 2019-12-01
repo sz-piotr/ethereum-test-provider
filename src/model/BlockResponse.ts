274a27e74eb14aaabf672b5267581aa07a773631
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2019-12-01 22:23:15 +0100

    Remove unnecessary semicolons

--- src/model/BlockResponse.ts
+++ src/model/BlockResponse.ts
@@ -6,7 +6,7 @@ import Block from 'ethereumjs-block'
 import { bufferToInt, bufferToHex } from 'ethereumjs-util'
 import { bufferToAddress } from '../utils'
 
-export type BlockResponse = BlockResponseWithTxHashes | BlockResponseWithTxResponses;
+export type BlockResponse = BlockResponseWithTxHashes | BlockResponseWithTxResponses
 
 // https://github.com/ethers-io/ethers.js/blob/4ac08432b8e2c7c374dc4a0e141a39a369e2d430/src.ts/providers/base-provider.ts#L259
 export interface BlockResponseWithTxHashes {
--- src/model/FilterRequest.ts
+++ src/model/FilterRequest.ts
@@ -2,7 +2,7 @@
 import { Address, BlockTag, Hash } from './strings'
 
 // https://github.com/ethers-io/ethers.js/blob/4ac08432b8e2c7c374dc4a0e141a39a369e2d430/src.ts/providers/base-provider.ts#L376
-export type FilterRequest = FilterByRangeRequest | FilterByBlockRequest;
+export type FilterRequest = FilterByRangeRequest | FilterByBlockRequest
 
 // https://github.com/ethers-io/ethers.js/blob/4ac08432b8e2c7c374dc4a0e141a39a369e2d430/src.ts/providers/base-provider.ts#L363
 export interface FilterByRangeRequest {
